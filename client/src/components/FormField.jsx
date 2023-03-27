5f9e2404dc56f8007d5e8fecf7010255f9727fa5
sladeutley <slade.utley@gmail.com>
2023-03-27 09:29:35 -0700

    have the outline for client side set up

---
+++ client/src/components/FormField.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const FormField = ({ labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => { //destructure props we pass into this component
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
+        </label>
+        {isSurpriseMe && ( //only for FormFieleds that have isSurpriseMe prop on, we will render a button. if isSurpriseMe, then button
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#EcECF1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
+      />
+    </div>
+  )
+}
+
+export default FormField
